aca5ce71771b5c8281d7104713cb914e925676c1
João Pedro Freitas de Oliveira <jpfreitas15@hotmail.com>
2024-10-04 01:18:01 -0300

    Add types

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,8 @@
-import { Negociacao } from "./models/negociacao.js";
+import NegociacaoController from "./controllers/negociacao-controller.js";
 
-const bussines = new Negociacao(new Date(), 10, 100);
-console.log(bussines.valor)
-console.log(bussines.volume)
-
-
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(controller.adicionar());
+})
